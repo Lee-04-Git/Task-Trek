67b65f44b45f73d5691ff8e28f3380f4caac044a
Lee-04-Git <leemaalgraaff.github@gmail.com>
2024-12-17 14:05:39 +0200

    updated App component to manage and display tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,39 @@
-import React from 'react';
+import React, { useState } from "react";
 import "./App.css";
-import TaskForm from './components/TaskForm';
-import TaskColumn from './components/TaskColumn';
-import todoIcon from './assets/direct-hit.png';
-import doingIcon from './assets/glowing-star.png';
-import doneIcon from './assets/check-mark-button.png';
+import TaskForm from "./components/TaskForm";
+import TaskColumn from "./components/TaskColumn";
+import todoIcon from "./assets/direct-hit.png";
+import doingIcon from "./assets/glowing-star.png";
+import doneIcon from "./assets/check-mark-button.png";
 
 const App = () => {
+  const [tasks, setTasks] = useState([]);
+  console.log("tasks", tasks);
   return (
-    <div className='app'>
-      <TaskForm />
-      <main className='app_main'>
-        <TaskColumn title='To do' icon={todoIcon} />
-        <TaskColumn title='Doing' icon={doingIcon} />
-        <TaskColumn title='Done' icon={doneIcon} />
+    <div className="app">
+      <TaskForm setTasks={setTasks} />
+      <main className="app_main">
+        <TaskColumn
+        title="To do" 
+        icon={todoIcon}
+        tasks={tasks}
+        status="todo"
+        />
+        <TaskColumn
+          title="Doing"
+          icon={doingIcon}
+          tasks={tasks}
+          status="doing"
+        />
+        <TaskColumn 
+        title="Done"
+        icon={doneIcon}
+        tasks={tasks}
+        status="done"
+        />
       </main>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
