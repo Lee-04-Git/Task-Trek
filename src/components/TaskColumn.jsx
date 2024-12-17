4230f6f57b821260f9748f18d1850b58f6621e5a
Lee-04-Git <leemaalgraaff.github@gmail.com>
2024-12-17 14:06:26 +0200

    updated TaskColumn to dynamically display tasks based on status

--- src/components/TaskColumn.jsx
+++ src/components/TaskColumn.jsx
@@ -1,18 +1,23 @@
-import React from 'react';
-import Todo from "../assets/direct-hit.png"
+import React from "react";
+import Todo from "../assets/direct-hit.png";
 import "./TaskColumn.css";
-import TaskCard from './TaskCard';
+import TaskCard from "./TaskCard";
 
-const TaskColumn = ({title, icon}) => {
+const TaskColumn = ({ title, icon, tasks, status }) => {
   return (
-    <section className='task_column'>
-        <h2 className="task_column_heading">
-            <img className='task_column_icon' src={icon}
-            alt="" />{title}
-          </h2>
-          <TaskCard />
+    <section className="task_column">
+      <h2 className="task_column_heading">
+        <img className="task_column_icon" src={icon} alt="" />
+        {title}
+      </h2>
+      {tasks.map(
+        (task, index) =>
+          task.status === status && (
+            <TaskCard key={index} title={task.task} tags={task.tags} />
+          )
+      )}
     </section>
-  )
-}
+  );
+};
 
-export default TaskColumn
+export default TaskColumn;
