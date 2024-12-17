8479e79077a937fa33a4a8b4271749c2519c6166
Lee-04-Git <leemaalgraaff.github@gmail.com>
2024-12-17 12:20:10 +0200

    added state handling for task input and status change

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./TaskForm.css";
 import Tag from './Tag';
 
 const TaskForm = () => {
+    const [task,setTask] = useState("");
+    const [status, setStatus] = useState("todo")
+
+    const handleTaskChange = e => {
+        setTask(e.target.value);
+    };
+
+    const handleStatusChange = e => {
+        setStatus(e.target.value);
+    };
+
   return (
     <header className='app_header'>
         <form>
-            <input type="text" className="task_input"
-            placeholder='Enter your task' />
+            <input type="text"
+             className="task_input"
+            placeholder='Enter your task'
+            onChange={handleTaskChange}
+        />
 
             <div className='task_form_bottom_line'>
                 
@@ -19,7 +33,8 @@ const TaskForm = () => {
                 </div>
 
                 <div>
-                <select className='task_status'>
+                <select className='task_status'
+                onChange={handleStatusChange}>
                     <option value="todo">To do</option>
                     <option value="doing">Doing</option>
                     <option value="done">Done</option>
